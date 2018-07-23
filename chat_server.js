8eb69f8eaf6afb0f4a4a496f46e6270ad9254dc4
Sam Spencer <sspencerbtv@gmail.com>
2018-07-23 14:50:50 -0400

    have posts showing on page

--- chat_server.js
+++ chat_server.js
@@ -1,39 +1,43 @@
 //setup requirements
 const http = require('http');
-const fs = require('fs');
 const mime = require('mime-types');
+const Assistant = require('./assistant')
 const port = process.env.PORT || 5000;
+
+let messages = [];
 
 http.createServer(handleRequest).listen(port);
 console.log("listening on port:" + port);
 
 function handleRequest (request, response) {
-    console.log("request recieved!");
     console.log('request.url = ' + request.url);
     
-    let fileName;
-    let data;
-    let type = 'text/plain';
+    let assistant = new Assistant(request, response);
+    let path = assistant.path;
+    
 
     try {
-        if (request.url === '/') {
-            fileName = 'index.html';
+        if (path === '/') {
+            assistant.sendFile('index.html');
+        } else if (path === '/chat') {
+            console.log('Parsing the POST');
+            assistant.parsePostParams((params) => {
+                let message = {
+                    name: 'Anonymous',
+                    body: params.body,
+                    when: new Date().toISOString()
+                }
+                messages.push(message);
+                let data = JSON.stringify(messages);
+                let type = mime.lookup('json');
+                assistant.finishResponse(type, data);
+            })
         } else {
-            fileName = request.url.slice(1);
+            let fileName = path.slice(1);
+            assistant.sendFile(fileName);
         }
-        data = fs.readFileSync(fileName);
-        type = mime.lookup(fileName);
-        response.statusCode = 200;
+        
     } catch (error) {
-        console.log(error);
-        data = "error:" + error.toString();
-        response.statusCode = 404;
+        assistant.sendError(404, "Error: " + error.toString());
     }
-    
-    
-    let contentType = `${type}; charset=utf-8`
-
-    response.setHeader('Content-Type', contentType)
-    response.write(data)
-    response.end();   
 }
